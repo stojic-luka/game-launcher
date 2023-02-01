a77cd4be37629459a818e1f24969b5a6250e4a79
stojic-luka <124006893+stojic-luka@users.noreply.github.com>
2023-02-01 05:01:50 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace Game_Launcher {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.top_panel = new System.Windows.Forms.Panel();
+            this.profile_name_label = new System.Windows.Forms.Label();
+            this.profile_picturebox = new System.Windows.Forms.Panel();
+            this.search_games_textbox = new System.Windows.Forms.TextBox();
+            this.drag_panel = new System.Windows.Forms.Panel();
+            this.minimise_button_panel = new System.Windows.Forms.Panel();
+            this.minimise_button_label = new System.Windows.Forms.Label();
+            this.maximise_button_panel = new System.Windows.Forms.Panel();
+            this.maximise_button_label = new System.Windows.Forms.Label();
+            this.exit_button_panel = new System.Windows.Forms.Panel();
+            this.exit_button_label = new System.Windows.Forms.Label();
+            this.side_game_panel = new System.Windows.Forms.Panel();
+            this.add_launchers_button = new System.Windows.Forms.Button();
+            this.show_all_games_panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pages_browser = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.top_panel.SuspendLayout();
+            this.drag_panel.SuspendLayout();
+            this.minimise_button_panel.SuspendLayout();
+            this.maximise_button_panel.SuspendLayout();
+            this.exit_button_panel.SuspendLayout();
+            this.side_game_panel.SuspendLayout();
+            this.show_all_games_panel.SuspendLayout();
+            this.pages_browser.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // top_panel
+            // 
+            this.top_panel.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.top_panel.Controls.Add(this.button1);
+            this.top_panel.Controls.Add(this.profile_name_label);
+            this.top_panel.Controls.Add(this.profile_picturebox);
+            this.top_panel.Controls.Add(this.search_games_textbox);
+            this.top_panel.Controls.Add(this.drag_panel);
+            this.top_panel.Location = new System.Drawing.Point(0, 0);
+            this.top_panel.Name = "top_panel";
+            this.top_panel.Size = new System.Drawing.Size(1419, 70);
+            this.top_panel.TabIndex = 0;
+            // 
+            // profile_name_label
+            // 
+            this.profile_name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.profile_name_label.Location = new System.Drawing.Point(1168, 28);
+            this.profile_name_label.Name = "profile_name_label";
+            this.profile_name_label.Size = new System.Drawing.Size(205, 35);
+            this.profile_name_label.TabIndex = 8;
+            this.profile_name_label.Text = "profile username";
+            this.profile_name_label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // profile_picturebox
+            // 
+            this.profile_picturebox.BackColor = System.Drawing.SystemColors.Control;
+            this.profile_picturebox.Location = new System.Drawing.Point(1377, 28);
+            this.profile_picturebox.Name = "profile_picturebox";
+            this.profile_picturebox.Size = new System.Drawing.Size(35, 35);
+            this.profile_picturebox.TabIndex = 7;
+            // 
+            // search_games_textbox
+            // 
+            this.search_games_textbox.AutoCompleteCustomSource.AddRange(new string[] {
+            "steam",
+            "epic games",
+            "battle.net",
+            "counter strike global offensive",
+            "payday 2"});
+            this.search_games_textbox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.search_games_textbox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.search_games_textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.search_games_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_games_textbox.Location = new System.Drawing.Point(65, 35);
+            this.search_games_textbox.Name = "search_games_textbox";
+            this.search_games_textbox.Size = new System.Drawing.Size(295, 22);
+            this.search_games_textbox.TabIndex = 3;
+            // 
+            // drag_panel
+            // 
+            this.drag_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drag_panel.Controls.Add(this.minimise_button_panel);
+            this.drag_panel.Controls.Add(this.maximise_button_panel);
+            this.drag_panel.Controls.Add(this.exit_button_panel);
+            this.drag_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.drag_panel.Location = new System.Drawing.Point(0, 0);
+            this.drag_panel.Name = "drag_panel";
+            this.drag_panel.Size = new System.Drawing.Size(1419, 23);
+            this.drag_panel.TabIndex = 2;
+            this.drag_panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drag_panel_MouseDown);
+            // 
+            // minimise_button_panel
+            // 
+            this.minimise_button_panel.BackColor = System.Drawing.SystemColors.Control;
+            this.minimise_button_panel.Controls.Add(this.minimise_button_label);
+            this.minimise_button_panel.Location = new System.Drawing.Point(1310, 0);
+            this.minimise_button_panel.Name = "minimise_button_panel";
+            this.minimise_button_panel.Size = new System.Drawing.Size(21, 21);
+            this.minimise_button_panel.TabIndex = 7;
+            // 
+            // minimise_button_label
+            // 
+            this.minimise_button_label.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimise_button_label.ForeColor = System.Drawing.Color.Black;
+            this.minimise_button_label.Location = new System.Drawing.Point(2, 2);
+            this.minimise_button_label.Name = "minimise_button_label";
+            this.minimise_button_label.Size = new System.Drawing.Size(20, 20);
+            this.minimise_button_label.TabIndex = 1;
+            this.minimise_button_label.Tag = "";
+            this.minimise_button_label.Text = "--";
+            this.minimise_button_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.minimise_button_label.Click += new System.EventHandler(this.minimise_button_label_Click);
+            this.minimise_button_label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.navigation_button_label_MouseDown);
+            this.minimise_button_label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.navigation_button_label_MouseUp);
+            // 
+            // maximise_button_panel
+            // 
+            this.maximise_button_panel.BackColor = System.Drawing.SystemColors.Control;
+            this.maximise_button_panel.Controls.Add(this.maximise_button_label);
+            this.maximise_button_panel.Location = new System.Drawing.Point(1339, 0);
+            this.maximise_button_panel.Name = "maximise_button_panel";
+            this.maximise_button_panel.Size = new System.Drawing.Size(21, 21);
+            this.maximise_button_panel.TabIndex = 6;
+            // 
+            // maximise_button_label
+            // 
+            this.maximise_button_label.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maximise_button_label.ForeColor = System.Drawing.Color.Black;
+            this.maximise_button_label.Location = new System.Drawing.Point(1, 1);
+            this.maximise_button_label.Name = "maximise_button_label";
+            this.maximise_button_label.Size = new System.Drawing.Size(20, 20);
+            this.maximise_button_label.TabIndex = 1;
+            this.maximise_button_label.Tag = "";
+            this.maximise_button_label.Text = "⬜";
+            this.maximise_button_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.maximise_button_label.Click += new System.EventHandler(this.maximise_button_label_Click);
+            this.maximise_button_label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.navigation_button_label_MouseDown);
+            this.maximise_button_label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.navigation_button_label_MouseUp);
+            // 
+            // exit_button_panel
+            // 
+            this.exit_button_panel.BackColor = System.Drawing.SystemColors.Control;
+            this.exit_button_panel.Controls.Add(this.exit_button_label);
+            this.exit_button_panel.Location = new System.Drawing.Point(1366, 0);
+            this.exit_button_panel.Name = "exit_button_panel";
+            this.exit_button_panel.Size = new System.Drawing.Size(45, 21);
+            this.exit_button_panel.TabIndex = 5;
+            // 
+            // exit_button_label
+            // 
+            this.exit_button_label.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit_button_label.ForeColor = System.Drawing.Color.Black;
+            this.exit_button_label.Location = new System.Drawing.Point(2, 2);
+            this.exit_button_label.Name = "exit_button_label";
+            this.exit_button_label.Size = new System.Drawing.Size(43, 18);
+            this.exit_button_label.TabIndex = 0;
+            this.exit_button_label.Tag = "";
+            this.exit_button_label.Text = "X";
+            this.exit_button_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.exit_button_label.Click += new System.EventHandler(this.exit_button_label_Click);
+            this.exit_button_label.MouseDown += new System.Windows.Forms.MouseEventHandler(this.navigation_button_label_MouseDown);
+            this.exit_button_label.MouseUp += new System.Windows.Forms.MouseEventHandler(this.navigation_button_label_MouseUp);
+            // 
+            // side_game_panel
+            // 
+            this.side_game_panel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.side_game_panel.Controls.Add(this.add_launchers_button);
+            this.side_game_panel.Controls.Add(this.show_all_games_panel);
+            this.side_game_panel.Location = new System.Drawing.Point(0, 70);
+            this.side_game_panel.Name = "side_game_panel";
+            this.side_game_panel.Size = new System.Drawing.Size(65, 515);
+            this.side_game_panel.TabIndex = 1;
+            // 
+            // add_launchers_button
+            // 
+            this.add_launchers_button.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.add_launchers_button.FlatAppearance.BorderSize = 0;
+            this.add_launchers_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_launchers_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_launchers_button.Location = new System.Drawing.Point(0, 486);
+            this.add_launchers_button.Name = "add_launchers_button";
+            this.add_launchers_button.Size = new System.Drawing.Size(65, 25);
+            this.add_launchers_button.TabIndex = 0;
+            this.add_launchers_button.Text = "+";
+            this.add_launchers_button.UseVisualStyleBackColor = false;
+            // 
+            // show_all_games_panel
+            // 
+            this.show_all_games_panel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.show_all_games_panel.Controls.Add(this.label1);
+            this.show_all_games_panel.Location = new System.Drawing.Point(8, 8);
+            this.show_all_games_panel.Name = "show_all_games_panel";
+            this.show_all_games_panel.Size = new System.Drawing.Size(49, 49);
+            this.show_all_games_panel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "all";
+            // 
+            // pages_browser
+            // 
+            this.pages_browser.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.pages_browser.Controls.Add(this.tabPage1);
+            this.pages_browser.Controls.Add(this.tabPage2);
+            this.pages_browser.ItemSize = new System.Drawing.Size(10, 10);
+            this.pages_browser.Location = new System.Drawing.Point(71, 76);
+            this.pages_browser.Name = "pages_browser";
+            this.pages_browser.SelectedIndex = 0;
+            this.pages_browser.Size = new System.Drawing.Size(1341, 496);
+            this.pages_browser.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.pages_browser.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 14);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1333, 478);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 14);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1333, 478);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1423, 584);
+            this.Controls.Add(this.pages_browser);
+            this.Controls.Add(this.side_game_panel);
+            this.Controls.Add(this.top_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "w";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Resize += new System.EventHandler(this.setup_window);
+            this.top_panel.ResumeLayout(false);
+            this.top_panel.PerformLayout();
+            this.drag_panel.ResumeLayout(false);
+            this.minimise_button_panel.ResumeLayout(false);
+            this.maximise_button_panel.ResumeLayout(false);
+            this.exit_button_panel.ResumeLayout(false);
+            this.side_game_panel.ResumeLayout(false);
+            this.show_all_games_panel.ResumeLayout(false);
+            this.show_all_games_panel.PerformLayout();
+            this.pages_browser.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel top_panel;
+        private System.Windows.Forms.Panel side_game_panel;
+        private System.Windows.Forms.Panel show_all_games_panel;
+        private System.Windows.Forms.Panel drag_panel;
+        private System.Windows.Forms.Panel exit_button_panel;
+        private System.Windows.Forms.Label exit_button_label;
+        private System.Windows.Forms.Panel minimise_button_panel;
+        private System.Windows.Forms.Label minimise_button_label;
+        private System.Windows.Forms.Panel maximise_button_panel;
+        private System.Windows.Forms.Label maximise_button_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button add_launchers_button;
+        private System.Windows.Forms.TextBox search_games_textbox;
+        private System.Windows.Forms.Panel profile_picturebox;
+        private System.Windows.Forms.Label profile_name_label;
+        private System.Windows.Forms.TabControl pages_browser;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using Transitions;
+
+namespace Game_Launcher {
+    public partial class Form1 : Form {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void drag_panel_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+        private void setup_window(object sender, EventArgs e) {
+            this.AllowTransparency = false;
+            top_panel.Size = new Size(this.Width, 70);
+            side_game_panel.Size = new Size(65, this.Height - 70);
+            add_launchers_button.Location = new Point(0, this.Height - 95);
+            exit_button_panel.Location = new Point(this.Width - 46, 0);
+            maximise_button_panel.Location = new Point(this.Width - 68, 0);
+            minimise_button_panel.Location = new Point(this.Width - 90, 0);
+            profile_picturebox.Location = new Point(this.Width - 41, 28);
+            search_games_textbox.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, search_games_textbox.Width, search_games_textbox.Height, 4, 4));
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, this.Width, this.Height, 8, 8));
+            search_games_textbox.AutoCompleteCustomSource.Clear();
+            search_games_textbox.AutoCompleteCustomSource.AddRange(new string[] {
+                "Steam",
+                "Team Fortress 2",
+                "Payday2",
+                "Steam 2",
+                "Pay to me",
+                "Team crafted",
+                "Team nesto, ne znam"
+            });
+        }
+        private void load_apps() {
+            show_all_games_panel.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, show_all_games_panel.Width, show_all_games_panel.Height, 6, 6));
+            using (StreamReader file = File.OpenText(@"apps.json")) {
+                JObject jobject = JObject.Parse(file.ReadToEnd());
+                List<string> app_list = jobject.Value<JArray>("apps").ToObject<List<string>>();
+                foreach (string app_name in app_list) {
+                    Panel panel = panel_creator(app_name);
+                    int panel_num = side_game_panel.Controls.Count - 1;
+                    panel.Location = new Point(8, panel_num * 49 + (panel_num + 1) * 8);
+                    panel.BackColor = Color.MediumAquamarine;
+                    side_game_panel.Controls.Add(panel);
+                }
+            }
+        }
+        public Form1() {
+            InitializeComponent();
+            load_apps();
+        }
+        
+        FormWindowState state_before_minimize;
+        private void navigation_button_label_MouseDown(object sender, MouseEventArgs e) { var pnl = ((Label)sender).Parent; pnl.Location = new Point(pnl.Location.X, pnl.Location.Y + 1); }
+        private void navigation_button_label_MouseUp(object sender, MouseEventArgs e) { var pnl = ((Label)sender).Parent; if (pnl != null) pnl.Location = new Point(pnl.Location.X, pnl.Location.Y - 1); }
+        private void exit_button_label_Click(object sender, EventArgs e) { this.Close(); Environment.Exit(0); }
+        private void maximise_button_label_Click(object sender, EventArgs e) {
+            if (this.WindowState == FormWindowState.Minimized) this.WindowState = state_before_minimize;
+            else if (this.WindowState == FormWindowState.Normal) this.WindowState = FormWindowState.Maximized;
+            else this.WindowState = FormWindowState.Normal;
+        }
+        private void minimise_button_label_Click(object sender, EventArgs e) {
+            state_before_minimize = this.WindowState;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        bool draging = false; int currentY, starting_panel_pos; byte movement_speed = 200;
+        private void game_platform_panel_MouseDown(object sender, MouseEventArgs e) { draging = true; currentY = e.Y; starting_panel_pos = ((Panel)sender).Location.Y; ((Panel)sender).BringToFront(); }
+        private void game_platform_panel_MouseUp(object sender, MouseEventArgs e) { draging = false; Transition.run(sender, "Top", starting_panel_pos, new TransitionType_EaseInEaseOut(movement_speed)); }
+        private void game_platform_panel_MouseMove(object sender, MouseEventArgs e) {
+            if (draging) {
+                Panel current_panel = (Panel)sender, changing_panel;
+                current_panel.Top = current_panel.Top + (e.Y - currentY);
+                if (current_panel.Location.Y <= 56) { draging = false; Transition.run(current_panel, "Top", 65, new TransitionType_EaseInEaseOut(250)); }
+                if (starting_panel_pos - 28 > current_panel.Location.Y) {
+                    changing_panel = find_game_platform_panel_by_location(starting_panel_pos - 57, current_panel.Name);
+                    if (changing_panel != null) {
+                        Transition.run(changing_panel, "Top", starting_panel_pos, new TransitionType_EaseInEaseOut(movement_speed));
+                        starting_panel_pos = changing_panel.Location.Y;
+                    }
+                } else if (starting_panel_pos + 28 < current_panel.Location.Y) {
+                    changing_panel = find_game_platform_panel_by_location(starting_panel_pos + 57, current_panel.Name);
+                    if (changing_panel != null) {
+                        Transition.run(changing_panel, "Top", starting_panel_pos, new TransitionType_EaseInEaseOut(movement_speed));
+                        starting_panel_pos = changing_panel.Location.Y;
+                    }
+                }
+            }
+        }
+        private Panel find_game_platform_panel_by_location(int target_pos, string panel_name) {
+            foreach (var p in side_game_panel.Controls.OfType<Control>().ToArray())
+                if (p.Location.Y == target_pos && p.Name != "show_all_games_panel" && p.Name != panel_name)
+                    return (Panel)p;
+            return null;
+        }
+
+        private Panel panel_creator(string app_name) {
+            var panel = new Panel();
+            panel.Size = new Size(49, 49);
+            panel.Name = app_name;
+            panel.MouseDown += game_platform_panel_MouseDown;
+            panel.MouseUp += game_platform_panel_MouseUp;
+            panel.MouseMove += game_platform_panel_MouseMove;
+            panel.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel.Width, panel.Height, 6, 6));
+            var pictureBox = new PictureBox();
+            pictureBox.Location = new Point(5, 5);
+            pictureBox.Size = new Size(39, 39);
+            pictureBox.Enabled = false;
+            panel.Controls.Add(pictureBox);
+            return panel;
+        }
+    }
+}
